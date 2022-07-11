9833410ac323f1c9140422aa74d384dffc295084
Jakob Kramp <jakob.kramp@rwth-aachen.de>
2022-07-11 15:17:56 +0200

    change parameter in bash-script

--- scripts/jakob_bash_4.sh
+++ scripts/jakob_bash_4.sh
@@ -3,19 +3,19 @@
 
 echo "running emcee"
 START1="$(date +%s)"
-python emcee_jax_cosmo.py -num_steps 20 -nwalkers 16
+python emcee_jax_cosmo.py -num_steps 20000 -nwalkers 16
 
 echo "The emcee part of the code took: $[ $(date +%s) - ${START1} ]"
 
 echo "running ADVI"
 START2="$(date +%s)"
-python advi_jax_cosmo.py -num_samples 100000 -grad_steps 250 -stepsize 0.005
+python advi_jax_cosmo.py -num_samples 100000 -grad_steps 25000 -stepsize 0.005
 
 echo "The ADVI part of the code took: $[ $(date +%s) - ${START2} ]"
 
 echo "running NUTS"
 START3="$(date +%s)"
-python nuts_jax_cosmo.py -num_samples 200 -burnin 50 -nwalkers 16
+python nuts_jax_cosmo.py -num_samples 20000 -burnin 500 -nwalkers 16
 
 echo "The NUTS part of the code took: $[ $(date +%s) - ${START3} ]"
 
