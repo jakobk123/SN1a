e7663dfec2895d1e0c181fef0a6565b8b2ba4f6b
Santiago Casas <casas@physik.rwth-aachen.de>
2022-07-12 14:32:12 +0200

    updated bash script

--- scripts/jakob_bash_5.sh
+++ scripts/jakob_bash_5.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 
-echo "running emcee"
-START1="$(date +%s)"
-python emcee_jax_cosmo.py -num_steps 25000 -nwalkers 16
-
-echo "The emcee part of the code took: $[ $(date +%s) - ${START1} ]"
 
 echo "running ADVI"
 START2="$(date +%s)"
@@ -18,6 +13,16 @@ START3="$(date +%s)"
 python nuts_jax_cosmo.py -num_samples 25000 -burnin 500 -nwalkers 16
 
 echo "The NUTS part of the code took: $[ $(date +%s) - ${START3} ]"
+
+rune=$1
+
+if [[ "$rune" == "emcee" ]]
+then
+echo "running emcee"
+START1="$(date +%s)"
+python emcee_jax_cosmo.py -num_steps 25000 -nwalkers 16
+echo "The emcee part of the code took: $[ $(date +%s) - ${START1} ]"
+fi
 
 
 echo "runs finished"
